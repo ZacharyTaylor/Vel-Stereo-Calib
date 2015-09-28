81366bf8bb015c88cebb3ee90fd1be783bb531a5
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2015-09-28 12:43:51 +1000

    Improved layout and added classes

--- src/Vel-Stereo-Calib.cpp
+++ src/Vel-Stereo-Calib.cpp
@@ -9,166 +9,301 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/calib3d/calib3d.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <pcl/io/ply_io.h>
 #include <pcl/common/transforms.h>
 
-#define DEFAULT_BLOCK_SIZE 5
+#define DEFAULT_PATCH_SIZE 5
 #define IMAGE_WIDTH 1024
 #define IMAGE_HEIGHT 768
 
-bool invalidImageLoc(const std::pair<cv::Point2f, cv::Point2f> point)
+struct CameraProperties
 {
-  const unsigned int min_width = DEFAULT_BLOCK_SIZE;
-  const unsigned int min_height = DEFAULT_BLOCK_SIZE;
+  const Eigen::Affine3f intrinsics;
+  Eigen::Affine3f tform;
 
-  const unsigned int max_width = IMAGE_WIDTH - DEFAULT_BLOCK_SIZE;
-  const unsigned int max_height = IMAGE_HEIGHT - DEFAULT_BLOCK_SIZE;
-
-  if((point.first.x >= min_width) &&
-      (point.first.y >= min_height) &&
-      (point.first.x < max_width) &&
-      (point.first.y < max_height) &&
-      (point.second.x >= min_width) &&
-      (point.second.y >= min_height) &&
-      (point.second.x < max_width) &&
-      (point.second.y < max_height))
+  CameraProperties(float fx, float fy, float cx, float cy, Eigen::Affine3f tform_in = Eigen::Affine3f::Identity()) :
+      intrinsics(initIntrinsics(fx, fy, cx, cy)), tform(tform_in)
   {
-    return false;
   }
-  else
-  {
-    return true;
-  }
-}
 
-class VelStereoMatcher
+  CameraProperties(Eigen::Affine3f intrinsics_in, Eigen::Affine3f tform_in = Eigen::Affine3f::Identity()) :
+      intrinsics(intrinsics_in), tform(tform_in)
+  {
+  }
+
+private:
+  Eigen::Affine3f initIntrinsics(float fx, float fy, float cx, float cy)
+  {
+    Eigen::Affine3f mat = Eigen::Affine3f::Identity();
+
+    mat(0, 0) = fx;
+    mat(1, 1) = fy;
+    mat(0, 2) = cx;
+    mat(1, 2) = cy;
+
+    return mat;
+  }
+};
+
+struct StereoProperties : private CameraProperties
 {
-  cv::Mat camera_intrinsics;
   float baseline;
 
-  unsigned int block_size = DEFAULT_BLOCK_SIZE;
-
-  std::vector<cv::Mat> left_images;
-  std::vector<cv::Mat> right_images;
-
-  std::vector<pcl::PointCloud<pcl::PointXYZ> > vel_scans;
-
-  cv::Mat VecToTformMat(const std::vector<float>& r_vec, const std::vector<float>& t_vec)
+  CameraProperties get_left_camera(void) const
   {
-    Eigen::AngleAxis<float> aa(angle_in_radian, Vector3f(ax,ay,az));
-
-    cv::Mat tform = cv::Mat::zeros(4, 4, CV_32F);
-    cv::Rodrigues(r_vec, tform);
-
-    for(size_t i = 0; i < 3; i++)
-      tform.at<float>(i,3) = t_vec[i];
-    tform.at<float>(3,3) = 1;
-
-    return tform;
+    return CameraProperties(this->intrinsics, this->tform);
   }
 
-  std::vector<cv::Point2f> ProjectScan(const std::vector<cv::Point3f>& scan,
-                                        const std::vector<float>& r_vec,
-                                        const std::vector<float>& t_vec)
+  CameraProperties get_right_camera(void) const
   {
-    auto tform = VecToTformMat(r_vec, t_vec);
-
-    cv::Point3f a;
-
-
-
-    return projected;
+    auto tform_right = this->tform;
+    tform_right.translation() << -this->baseline, 0.0, 0.0;
+    return CameraProperties(this->intrinsics, tform_right);
   }
 
-public:
-
-  void printProjectedSize(void)
+  void setLeftTform(const Eigen::Affine3f& tform_in)
   {
-    auto projected = ProjectScan(vel_scans[0], {-1.5,0,0},{0,0,0});
-
-    cv::Mat out = cv::Mat::zeros(IMAGE_HEIGHT, IMAGE_WIDTH, CV_32F);
-    for(auto &i : projected){
-      size_t x = i.first.x;
-      size_t y = i.first.y;
-      out.at<float>(y,x) = 1;
-    }
-    std::cout << projected.size() << std::endl;
-
-    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
-    cv::imshow( "Display window", out );                   // Show our image inside it.
-
-    cv::waitKey(0);
+    tform = tform_in;
   }
 
-  void setBlockSize(const unsigned int block_size_in)
+  StereoProperties(float fx, float fy, float cx, float cy, float baseline_in, Eigen::Affine3f tform_in =
+                       Eigen::Affine3f::Identity()) :
+      CameraProperties(fx, fy, cx, cy, tform_in), baseline(baseline_in)
   {
-    block_size = block_size_in;
   }
+};
 
-  void setCameraParameters(const float baseline_in,
-                           const float fx,
-                           const float fy,
-                           const float cx,
-                           const float cy)
+struct ProjectedPatch
+{
+  cv::Point2f point;
+  float intensity;
+  cv::Mat patch;
+  bool valid;
+
+  ProjectedPatch(pcl::PointXYZI raw_point, const cv::Mat& image, const size_t patch_size, CameraProperties camera)
   {
-    baseline = baseline_in;
 
-    camera_intrinsics = cv::Mat::zeros(3, 3, CV_32F);
-    camera_intrinsics.at<float>(0, 0) = fx;
-    camera_intrinsics.at<float>(1, 1) = fy;
-    camera_intrinsics.at<float>(0, 2) = cx;
-    camera_intrinsics.at<float>(1, 2) = cy;
-    camera_intrinsics.at<float>(2, 2) = 1;
-  }
+    raw_point = pcl::transformPoint(raw_point, camera.tform);
 
-  int addFrame(const std::string left_image_path,
-               const std::string right_image_path,
-               const std::string vel_scan_path)
-  {
-    //load data
-    std::cout << left_image_path << std::endl;
-    cv::Mat left_image = cv::imread(left_image_path, CV_LOAD_IMAGE_GRAYSCALE);
-    if(!left_image.data)
+    this->intensity = raw_point.intensity;
+
+    if (!(this->valid = (raw_point.z > 0)))
+      return;
+
+    //project onto plane
+    raw_point = pcl::transformPoint(raw_point, camera.intrinsics);
+    this->point.x = raw_point.x / raw_point.z;
+    this->point.y = raw_point.y / raw_point.z;
+
+    //check if inside image
+    if ((this->point.x < patch_size) || (this->point.y < patch_size)
+        || (this->point.x >= (image.size().width - patch_size))
+        || (this->point.y >= (image.size().height - patch_size)))
     {
-      std::cout << "Could not open or find the left image" << std::endl;
-      return -1;
+      this->valid = false;
+      return;
     }
 
-    cv::Mat right_image = cv::imread(right_image_path, CV_LOAD_IMAGE_GRAYSCALE);
-    if(!right_image.data)
-    {
-      std::cout << "Could not open or find the right image" << std::endl;
-      return -1;
-    }
-
-    pcl::PointCloud<pcl::PointXYZ> vel_scan;
-    if (pcl::io::loadPLYFile(vel_scan_path, vel_scan) < 0)
-    {
-      std::cout << "Could not open or find the velodyne scan" << std::endl;
-      return -1;
-    }
-
-    //store data
-    left_images.push_back(left_image);
-    right_images.push_back(right_image);
-    vel_scans.push_back(vel_scan);
-
-    return 0;
+    //extract patch
+    cv::Rect roi = cv::Rect(this->point.x - patch_size, this->point.y - patch_size, patch_size, patch_size);
+    this->patch = image(roi);
   }
 
 };
 
+struct ProjectedStereo
+{
+  ProjectedPatch left;
+  ProjectedPatch right;
+
+  ProjectedStereo(pcl::PointXYZI raw_point, const cv::Mat& image_left, const cv::Mat& image_right,
+                  const size_t patch_size, const StereoProperties& stereo) :
+      left(raw_point, image_left, patch_size, stereo.get_left_camera()), right(raw_point, image_right, patch_size,
+                                                                               stereo.get_right_camera())
+  {
+  }
+
+  bool valid(void) const
+  {
+    return this->left.valid && this->right.valid;
+  }
+
+  cv::Scalar meanAbsDiff(void) const
+  {
+    cv::Mat diff;
+    cv::absdiff(left.patch, right.patch, diff);
+    return cv::mean(diff);
+  }
+};
+
+struct DataFrame
+{
+  cv::Mat image_left;
+  cv::Mat image_right;
+  pcl::PointCloud<pcl::PointXYZI> vel_scan;
+
+  DataFrame(const std::string image_left_path, const std::string image_right_path, const std::string vel_scan_path) :
+      image_left(loadImage(image_left_path)), image_right(loadImage(image_right_path)), vel_scan(
+          loadVelScan(vel_scan_path))
+  {
+  }
+
+private:
+
+  cv::Mat loadImage(const std::string image_path)
+  {
+    auto image = cv::imread(image_path, CV_LOAD_IMAGE_GRAYSCALE);
+    if (!image.data)
+    {
+      throw std::runtime_error("Could not open or find the left image");
+    }
+
+    return image;
+  }
+
+  pcl::PointCloud<pcl::PointXYZI> loadVelScan(const std::string vel_scan_path)
+  {
+    pcl::PointCloud<pcl::PointXYZI> vel_scan;
+    if (pcl::io::loadPLYFile(vel_scan_path, vel_scan) < 0)
+    {
+      throw std::runtime_error("Could not open or find the velodyne scan");
+    }
+
+    return vel_scan;
+  }
+};
+
+class VelStereoMatcher
+{
+
+  cv::Scalar evalStereoError(const std::vector<std::vector<ProjectedStereo> >& proj_stereo)
+  {
+    cv::Scalar err;
+    size_t len = 0;
+    for (auto &points : proj_stereo)
+    {
+      for (auto &point : points)
+      {
+        if (point.valid())
+        {
+          err += point.meanAbsDiff();
+          ++len;
+        }
+      }
+    }
+
+    cv::Scalar result;
+    cv::divide(err, len, result);
+
+    return result;
+  }
+
+public:
+
+  StereoProperties stereo_prop;
+  unsigned int patch_size;
+  std::vector<DataFrame> frames;
+
+  VelStereoMatcher(float fx, float fy, float cx, float cy, float baseline, size_t patch_size_in = DEFAULT_PATCH_SIZE) :
+      stereo_prop(fx, fy, cx, cy, baseline), patch_size(patch_size_in)
+  {
+  }
+
+  void addFrame(const std::string image_left_path, const std::string image_right_path, const std::string vel_scan_path)
+  {
+    frames.push_back(DataFrame(image_left_path,image_right_path,vel_scan_path));
+  }
+
+  cv::Scalar evalTform(const Eigen::Affine3f& tform, bool plot = false)
+  {
+    this->stereo_prop.setLeftTform(tform);
+    std::vector<std::vector<ProjectedStereo> > proj_stereo_frames;
+
+    for (auto &frame : this->frames)
+    {
+      std::vector<ProjectedStereo> proj_stereo_frame;
+      for (auto &point : frame.vel_scan)
+      {
+        proj_stereo_frame.push_back(
+            ProjectedStereo(point, frame.image_left, frame.image_right, this->patch_size, this->stereo_prop));
+      }
+
+      proj_stereo_frames.push_back(proj_stereo_frame);
+
+      if (plot && proj_stereo_frames.size() == 1)
+      {
+        PlotProject(proj_stereo_frame, frame.image_left, frame.image_right);
+      }
+
+    }
+    return evalStereoError(proj_stereo_frames);
+  }
+
+  void PlotProject(std::vector<ProjectedStereo> projected, cv::Mat image_left, cv::Mat image_right)
+  {
+    cv::Mat vel_image_left(image_left.size(), image_left.type());
+    vel_image_left.setTo(0);
+    cv::Mat vel_image_right(image_right.size(), image_right.type());
+    vel_image_right.setTo(0);
+
+    for (auto &i : projected)
+    {
+      if (i.left.valid)
+      {
+        size_t x = i.left.point.x;
+        size_t y = i.left.point.y;
+        uchar v = i.left.intensity;
+        vel_image_left.at<uchar>(y, x) = v;
+      }
+      if (i.right.valid)
+      {
+        size_t x = i.right.point.x;
+        size_t y = i.right.point.y;
+        uchar v = i.right.intensity;
+        vel_image_right.at<uchar>(y, x) = v;
+      }
+    }
+
+    cv::dilate(vel_image_left, vel_image_left, cv::Mat(), cv::Point(-1, -1), 2, 1, 1);
+    cv::dilate(vel_image_right, vel_image_right, cv::Mat(), cv::Point(-1, -1), 2, 1, 1);
+
+    image_left /= 2;
+    image_left += 2*vel_image_left;
+
+    image_right /= 2;
+    image_right += 2*vel_image_right;
+
+    cv::namedWindow("left window", cv::WINDOW_AUTOSIZE);
+    cv::imshow("left window", image_left);
+    cv::namedWindow("right window", cv::WINDOW_AUTOSIZE);
+    cv::imshow("right window", image_right);
+
+    cv::waitKey(0);
+  }
+
+  void printProjectedSize(void)
+  {
+    Eigen::Affine3f tform = Eigen::Affine3f::Identity();
+    tform.rotate(Eigen::AngleAxisf(M_PI / 2, Eigen::Vector3f::UnitZ()));
+    tform.rotate(Eigen::AngleAxisf(-M_PI / 2, Eigen::Vector3f::UnitY()));
+    tform.rotate(Eigen::AngleAxisf(-0.9, Eigen::Vector3f::UnitZ()));
+
+    evalTform(tform, true);
+  }
+};
+
 int main(int argc, char* argv[])
 {
-  VelStereoMatcher matcher;
-  float baseline = 0.119658, fx = 1024*0.436783, fy = 768*0.582378, cx = 1024*0.502834, cy = 768*0.527529;
-  matcher.setCameraParameters(baseline,fx,fy,cx,cy);
-  matcher.addFrame(argv[1],argv[2],argv[3]);
+  float baseline = 0.119658, fx = 1024 * 0.436783, fy = 768 * 0.582378, cx = 1024 * 0.502834, cy = 768 * 0.527529;
+  VelStereoMatcher matcher(fx, fy, cx, cy, baseline);
+  matcher.addFrame(argv[1], argv[2], argv[3]);
   matcher.printProjectedSize();
 
 }
